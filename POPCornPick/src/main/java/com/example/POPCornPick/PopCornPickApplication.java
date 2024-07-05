538fc266c986aafa9d341d06a41ecf75a72bb7aa
Kim DongHwi <hwi9158@gmail.com>
2024-07-05 10:26:09 +0900

    aaa

---
+++ POPCornPick/src/main/java/com/example/POPCornPick/PopCornPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.POPCornPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopCornPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopCornPickApplication.class, args);
+	}
+
+}
